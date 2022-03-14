688b84036cbff51db1af902945b7845de186c10e
vgcrdv <88246112+vgcrdv@users.noreply.github.com>
2022-03-14 20:52:12 +0000

    Add files via upload

---
+++ DuplTwi_R0_0_var.py
@@ -0,0 +1,66 @@
+url_api = "https://gql.twitch.tv/gql"
+
+cabecera_token = {
+            'Accept': '*/*',
+            'Accept-Encoding': 'gzip, deflate, br',
+            'Accept-Language': 'es-ES',
+            'Authorization': 'undefined',
+            'Client-Id': 'kimne78kx3ncx6brgo4mv6wki5h1ko',
+            'Connection': 'keep-alive',
+            'Content-Length': '662',
+            'Content-Type': 'text/plain;charset=UTF-8',
+            'Device-Id': '0xuvWh0J2XUXQo78MMqtsAdD4xGPZ1ON',
+            'Host': 'gql.twitch.tv',
+            'Origin': 'https://www.twitch.tv',
+            'Referer': 'https://www.twitch.tv/',
+            'sec-ch-ua': '"Chromium";v="93", " Not A;Brand";v="99", "Google Chrome";v="93"',
+            'sec-ch-ua-mobile': '?0',
+            'sec-ch-ua-platform': '"Windows"',
+            'Sec-Fetch-Dest': 'empty',
+            'Sec-Fetch-Mode': 'cors',
+            'Sec-Fetch-Site': 'same-site',
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
+                          'Chrome/92.0.4515.131 Safari/537.36'
+            }
+
+cabecera_m3u8 = {
+            'Accept': 'application/x-mpegURL, application/vnd.apple.mpegurl, application/json, text/plain',
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
+                          'Chrome/92.0.4515.131 Safari/537.36'
+            }
+
+cabecera_data = {
+            'Accept': '*/*',
+            'Accept-Encoding': 'gzip, deflate, br',
+            'Accept-Language': 'es-ES',
+            'Authorization': 'undefined',
+            'Client-Id': 'kimne78kx3ncx6brgo4mv6wki5h1ko',
+            'Connection': 'keep-alive',
+            'Content-Length': '287',
+            'Content-Type': 'text/plain;charset=UTF-8',
+            'Device-Id': '0xuvWh0J2XUXQo78MMqtsAdD4xGPZ1ON',
+            'Host': 'gql.twitch.tv',
+            'Origin': 'https://www.twitch.tv',
+            'Referer': 'https://www.twitch.tv/',
+            'sec-ch-ua': '"Chromium";v="93", " Not A;Brand";v="99", "Google Chrome";v="93"',
+            'sec-ch-ua-mobile': '?0',
+            'sec-ch-ua-platform': '"Windows"',
+            'Sec-Fetch-Dest': 'empty',
+            'Sec-Fetch-Mode': 'cors',
+            'Sec-Fetch-Site': 'same-site',
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
+                          'Chrome/92.0.4515.131 Safari/537.36'
+            }
+
+lst_nomSim = {
+    '|': '.l.',
+    '>': '.M.',
+    '<': '.m.',
+    '"': '.__.',
+    '?':  '.!.',
+    '*': '.x.',
+    ':': '.-.',
+    '/': '.).',
+    '\\': '.(.',
+    '\n': ' ',
+    }
